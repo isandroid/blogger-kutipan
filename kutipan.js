eb8dee3b4afe1c5d9b9304cfb172dc1d3c6cc4a4
Isa Mujahid Islam <isandroid@users.noreply.github.com>
2021-02-08 17:03:18 +0700

    Update kutipan.js

--- kutipan.js
+++ kutipan.js
@@ -1,21 +1,22 @@
 <script language='javascript'>//<![CDATA[
+function createFootnote(footnote, url, judul){
+    sumber="<li id=\"cite_note-"+footnote+"\"><a href=\"#cite_note-"+footnote+"-0\">^</a>&nbsp;";
+    if(judul && url) {
+        sumber+="<a href=\""+url+"\">"+judul+"</a>";
+    } else if(judul) {
+        sumber+=judul;
+    } else if(url) {
+        sumber+="<a href=\""+url+"\">"+url+"</a>";
+    }
+    sumber+="</li>";
+    $("#footnotes").append(sumber);
+}
 $(document).ready(function() {
     $(".blog-posts article .post-outer").append("<a name=\"footnotes\"><ol id=\"footnotes\"></ol></a>");
     footnote = 1;
-    $(".footnote").each(function() {
-        $(this).append("<sup><a id=\"cite_note-"+footnote+"-0\" href=\"#cite_note-"+footnote+"\">["+footnote+"]</a></sup>");
-        sumber="<li id=\"cite_note-"+footnote+"\">";
-        url=$(this).attr("sumber");
-        judul=$(this).attr("judul");
-        if(judul && url) {
-            sumber+="<a href=\"#cite_note-"+footnote+"-0\">^</a> <a href=\""+url+"\">"+judul+"</a>";
-        } else if(judul) {
-            sumber+="<a href=\"#cite_note-"+footnote+"-0\">^</a> " + judul;
-        } else if(url) {
-            sumber+="<a href=\"#cite_note-"+footnote+"-0\">^</a> <a href=\""+url+"\">"+url+"</a>";
-        }
-        sumber+="</li>";
-        $("#footnotes").append(sumber);
+    $("ref").each(function() {
+        createFootnote(footnote, $(this).attr("sumber"), $(this).attr("judul"));
+        $(this).replaceWith("<sup><a id=\"cite_note-"+footnote+"-0\" href=\"#cite_note-"+footnote+"\">["+footnote+"]</a></sup>");
         footnote++;
     });
 });//]]>
