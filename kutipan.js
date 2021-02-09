94c18e7c77bf680461c0f75c4b0f5a1de371bae7
Isa Mujahid Islam <isandroid@users.noreply.github.com>
2021-02-09 19:32:04 +0700

    Update kutipan.js

--- kutipan.js
+++ kutipan.js
@@ -1,3 +1,5 @@
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
+
 <script language='javascript'>//<![CDATA[
 function createFootnote(footnote, url, judul){
     sumber="<li id=\"cite_note-"+footnote+"\"><a href=\"#cite_note-"+footnote+"-0\">^</a>&nbsp;";
